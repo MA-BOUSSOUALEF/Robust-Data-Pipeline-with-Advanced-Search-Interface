d248f388144a50649d0499331b7cff909b04cb86
Adam RAMDANI <adam.ramdaniar04@gmail.com>
2025-03-08 00:00:01 +0100

    first version 1.0.0

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ut3': '#fbc900',
+      },
+    },
+  },
+  plugins: [],
+}
